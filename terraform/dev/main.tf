30ed09f296374c58f59b8b66e4b32550c95c73b1
Johnathan Aspinwall <jwaspin@gmail.com>
2025-06-11 09:55:48 -0600

    Add Terraform configuration files for Google provider and lifecycle management

---
+++ terraform/dev/main.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  project     = "luca-ledger-dev"
+  region      = "us-central1"
+}
+
+module "network" {
+  source            = "../modules/network"
+
+  project_id        = "luca-ledger-dev"
+  region            = "us-central1"
+
+  network_name                   = "luca-ledger-dev-vpc-network"
+  subnet_name                    = "luca-ledger-dev-subnet"
+  subnet_ip                      = "10.1.1.1/24"
+}
