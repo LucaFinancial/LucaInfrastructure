7dac1b22728d1007ca5060c31926c4e426543a1a
Johnathan Aspinwall <jwaspin@gmail.com>
2025-06-30 17:43:59 -0600

    remove unused IAM and storage modules from Terraform configuration

--- terraform/modules/storage/main.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "luca_ledger_web_app_bucket" {
-  project                     = var.project_id
-  name                        = var.bucket_name
-  location                    = var.region
-  force_destroy               = true
-  uniform_bucket_level_access = true
-}
