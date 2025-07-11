1daf1d95fe23303d480fdfcb30237c4aa1d99ff9
Johnathan Aspinwall <jwaspin@gmail.com>
2025-07-11 15:06:27 -0600

    Refactor variable descriptions in variables.tf for consistency and clarity; remove obsolete network_self_link variable.

--- terraform/modules/core/variables.tf
+++ terraform/modules/core/variables.tf
@@ -1,46 +1,68 @@
 variable "project_id" {
-  description = "The ID of the GCP project"
   type        = string
+  description = "The ID of the GCP project"
 }
 
 variable "region" {
-  description = "The region where resources will be deployed"
   type        = string
+  description = "The region where resources will be deployed"
 }
 
 variable "network_name" {
+  type        = string
   description = "The name of the VPC network"
-  type        = string
-}
-
-variable "network_self_link" {
-  description = "The self-link URL of the VPC network"
-  type        = string
 }
 
 variable "subnet_name" {
-  description = "The name of the subnet within the VPC"
   type        = string
+  description = "The name of the subnet within the VPC"
 }
 
 variable "subnet_ip" {
-  description = "The IP CIDR range of the subnet (e.g., 10.0.0.0/24)"
   type        = string
+  description = "The IP CIDR range of the subnet (e.g., 10.0.0.0/24)"
 }
 
 variable "db_version" {
-  description = "The Cloud SQL database version (e.g., POSTGRES_13)"
   type        = string
+  description = "The Cloud SQL database version (e.g., POSTGRES_13)"
 }
 
 variable "db_instance_name" {
-  description = "The name to assign to the Cloud SQL instance"
   type        = string
+  description = "The name to assign to the Cloud SQL instance"
 }
 
 variable "db_name" {
-  description = "The name of the initial database to create"
   type        = string
+  description = "The name of the initial database to create"
+}
+
+variable "db_admin_username" {
+  type        = string
+  description = "The admin username for the Cloud SQL instance"
+}
+
+variable "db_admin_password" {
+  type        = string
+  description = "The password for the database admin user"
+  sensitive   = true
+}
+
+variable "db_user_username" {
+  type        = string
+  description = "The username for the application-level database user"
+}
+
+variable "db_user_password" {
+  type        = string
+  description = "The password for the application-level database user"
+  sensitive   = true
+}
+
+variable "deletion_protection" {
+  type        = bool
+  description = "Whether to enable deletion protection on the Cloud SQL instance"
 }
 
 variable "authorized_networks" {
@@ -49,30 +71,4 @@ variable "authorized_networks" {
     name  = string
     value = string
   }))
-}
-
-variable "db_admin_username" {
-  description = "The admin username for the Cloud SQL instance"
-  type        = string
-}
-
-variable "db_admin_password" {
-  description = "The password for the database admin user"
-  type        = string
-  sensitive   = true
-}
-
-variable "db_user_username" {
-  description = "The username for the application-level database user"
-  type        = string
-}
-
-variable "db_user_password" {
-  description = "The password for the application-level database user"
-  type        = string
-}
-
-variable "deletion_protection" {
-  description = "Whether to enable deletion protection on the Cloud SQL instance"
-  type        = bool
 }
