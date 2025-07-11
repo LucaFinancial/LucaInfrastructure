639f1fd3db8a9395c2c4b85974f91761e04f26b5
Johnathan Aspinwall <jwaspin@gmail.com>
2025-07-11 15:04:29 -0600

    Refactor GCP infrastructure setup: consolidate modules, add lifecycle config, and enhance variable definitions

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -1,30 +1,79 @@
-variable "db_user_username" {
-  description = "Database user username"
+variable "project_id" {
   type        = string
+  description = "The ID of the GCP project"
 }
 
-variable "db_user_password" {
-  description = "Database user password"
+variable "region" {
   type        = string
-  sensitive   = true
+  description = "The region where resources will be deployed"
 }
 
-variable "db_admin_username" {
-  description = "Database admin user name"
+variable "network_name" {
   type        = string
-  sensitive   = true
+  description = "The name of the VPC network"
 }
 
-variable "db_admin_password" {
-  description = "Database admin password"
+variable "network_self_link" {
   type        = string
-  sensitive   = true
+  description = "The self-link URL of the VPC network"
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "The name of the subnet within the VPC"
+}
+
+variable "subnet_ip" {
+  type        = string
+  description = "The IP CIDR range of the subnet (e.g., 10.0.0.0/24)"
+}
+
+variable "db_version" {
+  type        = string
+  description = "The Cloud SQL database version (e.g., POSTGRES_13)"
+}
+
+variable "db_instance_name" {
+  type        = string
+  description = "The name to assign to the Cloud SQL instance"
+}
+
+variable "db_name" {
+  type        = string
+  description = "The name of the initial database to create"
 }
 
 variable "authorized_networks" {
-  description = "Networks to add to the white list for allowed access"
+  description = "List of external networks authorized to connect to the database"
   type = list(object({
     name  = string
     value = string
   }))
+}
+
+variable "db_admin_username" {
+  type        = string
+  description = "The admin username for the Cloud SQL instance"
+}
+
+variable "db_admin_password" {
+  type        = string
+  description = "The password for the database admin user"
+  sensitive   = true
+}
+
+variable "db_user_username" {
+  type        = string
+  description = "The username for the application-level database user"
+}
+
+variable "db_user_password" {
+  type        = string
+  description = "The password for the application-level database user"
+  sensitive   = true
+}
+
+variable "deletion_protection" {
+  type        = bool
+  description = "Whether to enable deletion protection on the Cloud SQL instance"
 }
--- terraform/modules/core/outputs.tf
+++ terraform/modules/core/outputs.tf
@@ -1,11 +1,3 @@
 output "network_self_link" {
   value = google_compute_network.vpc_network.self_link
 }
-
-output "instance_connection_name" {
-  value = google_sql_database_instance.db_instance.connection_name
-}
-
-output "pg_private_ip" {
-  value = google_sql_database_instance.db_instance.private_ip_address
-}
