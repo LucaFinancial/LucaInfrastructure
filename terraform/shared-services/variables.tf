2e86efa287fb609e1cff3e749bee429dad1003da
Johnathan Aspinwall <jwaspin@gmail.com>
2025-07-21 17:53:25 -0600

    Add initial Terraform configuration for shared services environment

---
+++ terraform/shared-services/variables.tf
@@ -0,0 +1,14 @@
+variable "env" {
+  type        = string
+  description = "The environment"
+}
+
+variable "project_id" {
+  type        = string
+  description = "The ID of the GCP project"
+}
+
+variable "region" {
+  type        = string
+  description = "The region where resources will be deployed"
+}
