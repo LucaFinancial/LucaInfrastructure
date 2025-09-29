e537c2f8468a00e731b932fe76f4d4a904967b3e
Johnathan Aspinwall <jwaspin@gmail.com>
2025-09-29 16:48:33 -0600

    Remove unused core module and associated variables from dev and prod configurations

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,27 +1,3 @@
-module "core" {
-  source = "../modules/core"
-
-  project_id            = var.project_id
-  region                = var.region
-  
-  network_name          = var.network_name
-  subnet_name           = var.subnet_name
-  subnet_ip             = var.subnet_ip
-  
-  db_version            = var.db_version
-  db_instance_name      = var.db_instance_name
-  db_name               = var.db_name
-  
-  db_admin_username     = var.db_admin_username
-  db_admin_password     = var.db_admin_password
-  
-  db_user_username      = var.db_user_username
-  db_user_password      = var.db_user_password
-  
-  deletion_protection   = var.deletion_protection
-  authorized_networks   = var.authorized_networks
-}
-
 module "web-app-gcs" {
   source = "../modules/web-app-gcs"
 
