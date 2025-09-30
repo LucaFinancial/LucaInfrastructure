37a170aea41af65dc4b41fd1bb69a2e986434bde
Johnathan Aspinwall <jwaspin@gmail.com>
2025-09-29 22:22:00 -0600

    Add Cloud SQL and GCS infrastructure with service accounts and triggers

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,17 +1,15 @@
-module "web-app-gcs" {
-  source = "../modules/web-app-gcs"
+module "webapp-gcs" {
+  source = "../modules/webapp-gcs"
 
   env           = var.env
   project_id    = var.project_id
   region        = var.region
 
   service_account_name_gcs = "cloud-build-sa-gcs"
-  service_name_gcs  = "luca-ledger-dev-web-app-gcs"
+  service_name_gcs  = "luca-ledger-dev-webapp-gcs"
 
   branch_pattern  = ".*"
-  bucket_name     = "luca-ledger-dev-web-app"
+  bucket_name     = "luca-ledger-devops-dev-webapp"
 
-  ssl_domains = ["dev.lucaledger.app"]
+  ssl_domains = ["gcs.dev.lucaledger.app"]
 }
-
-#module "web-app-cloud-run" {}
