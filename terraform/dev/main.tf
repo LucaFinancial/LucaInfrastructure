97faa6f0bdd62a2add771a29f4a5a47e45ae6967
Johnathan Aspinwall <jwaspin@gmail.com>
2025-09-30 12:49:02 -0600

    Update module source and service account names in main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,12 +1,12 @@
 module "webapp-gcs" {
-  source = "../modules/webapp-gcs"
+  source = "../services/webapp-gcs"
 
   env           = var.env
   project_id    = var.project_id
   region        = var.region
 
-  service_account_name_gcs = "cloud-build-sa-gcs"
-  service_name_gcs  = "luca-ledger-dev-webapp-gcs"
+  service_account_name_gcs = "build-sa-gcs"
+  service_name_gcs  = "luca-ledger-devops-dev-webapp-gcs"
 
   branch_pattern  = ".*"
   bucket_name     = "luca-ledger-devops-dev-webapp"
