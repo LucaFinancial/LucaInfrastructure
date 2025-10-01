2e050f7fc06b97f8b806fcb358fdddfcfd849a04
Johnathan Aspinwall <jwaspin@gmail.com>
2025-09-30 22:55:39 -0600

    Update GCS service account and service name variables in locals configuration

--- terraform/prod/locals.tf
+++ terraform/prod/locals.tf
@@ -4,8 +4,8 @@ locals {
   region     = "us-central1"
 
   # GCS Variables
-  service_account_name_gcs = "luca-ledger-devops-prod-build-sa-gcs"
-  service_name_gcs         = "luca-ledger-devops-prod-webapp-gcs"
+  service_account_name_gcs = "ll-prod-build-sa-gcs"
+  service_name_gcs         = "ll-prod-webapp-gcs"
   branch_pattern = ".*"
   bucket_name = "luca-ledger-devops-prod-webapp"
   ssl_domains = ["gcs.lucaledger.app", "lucaledger.app"]
