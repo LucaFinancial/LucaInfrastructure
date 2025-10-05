f2c1c729d2eb370851ce268a5a525ae107c7ed3e
Johnathan Aspinwall <jwaspin@gmail.com>
2025-10-05 12:00:48 -0600

    Update branch pattern in GCS variables to restrict to main branch only

--- terraform/prod/locals.tf
+++ terraform/prod/locals.tf
@@ -6,7 +6,7 @@ locals {
   # GCS Variables
   service_account_name_gcs = "ll-prod-build-sa-gcs"
   service_name_gcs         = "ll-prod-webapp-gcs"
-  branch_pattern = ".*"
+  branch_pattern = "^main$"
   bucket_name = "luca-ledger-devops-prod-webapp"
   ssl_domains = ["gcs.lucaledger.app", "lucaledger.app"]
 }
