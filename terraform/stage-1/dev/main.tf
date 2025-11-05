e150f6c92105cc3c7e131098caee9c78e1a2ecb3
Johnathan Aspinwall <jwaspin@gmail.com>
2025-11-04 20:41:01 -0700

    Remove commented-out webapp-cloud-run module and update version to 1.5.0

--- terraform/stage-1/dev/main.tf
+++ terraform/stage-1/dev/main.tf
@@ -1,18 +1,3 @@
-# module "webapp-cloud-run" {
-#   source = "../services/webapp-cloud-run"
-
-#   env             = local.env
-#   project_id      = local.project_id
-#   region          = local.region
-#   branch_pattern  = local.branch_pattern
-
-#   service_name          = "lucaledger-dev-webapp-run"
-#   service_account_name  = "ll-dev-build-sa-cloudrun"
-#   trigger_name          = "lucaledger-dev-run"
-#   ssl_domains           = ["run-dev.lucaledger.app"]
-#   repository_id         = "lucaledger-dev-run"
-# }
-
 module "webapp-gcs" {
   source = "../services/webapp-gcs"
 
@@ -26,19 +11,4 @@ module "webapp-gcs" {
   bucket_name           = "lucaledger-dev-webapp"
   trigger_name          = "lucaledger-dev-gcs"
   ssl_domains           = ["dev.lucaledger.app"]
-}
-
-module "webapp-gcs-v2-beta" {
-  source = "../services/webapp-gcs"
-
-  env         = local.env
-  project_id  = local.project_id
-  region      = local.region
-
-  service_name          = "lucaledger-dev-gcs-v2-beta"
-  service_account_name  = "build-sa-gcs-v2-beta"
-  branch_pattern        = "release/v-2-0-0"
-  bucket_name           = "lucaledger-dev-v2-beta"
-  trigger_name          = "lucaledger-dev-v2-beta"
-  ssl_domains           = ["v2-beta.lucaledger.app"]
 }
