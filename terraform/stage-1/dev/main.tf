324b8f39c28fc4c19a1b804baa83f5bb1118d5a2
Johnathan Aspinwall <jwaspin@gmail.com>
2025-11-04 20:30:53 -0700

    Remove webapp-gcs-v1-beta module configuration

--- terraform/stage-1/dev/main.tf
+++ terraform/stage-1/dev/main.tf
@@ -28,21 +28,6 @@ module "webapp-gcs" {
   ssl_domains           = ["dev.lucaledger.app"]
 }
 
-module "webapp-gcs-v1-beta" {
-  source = "../services/webapp-gcs"
-
-  env         = local.env
-  project_id  = local.project_id
-  region      = local.region
-
-  service_name          = "lucaledger-dev-gcs-v1-beta"
-  service_account_name  = "build-sa-gcs-v1-beta"
-  branch_pattern        = "v1/*"
-  bucket_name           = "lucaledger-dev-v1-beta"
-  trigger_name          = "lucaledger-dev-v1-beta"
-  ssl_domains           = ["v1-beta.lucaledger.app"]
-}
-
 module "webapp-gcs-v2-beta" {
   source = "../services/webapp-gcs"
 
