12f4bdb8c4998fa92fdc1c889b62557a566b02d5
Johnathan <jwaspin@users.noreply.github.com>
2025-12-06 22:50:09 -0700

    Release v2 (#20)
    * Refactor webapp-gcs module configuration and remove webapp-v2 module
    
    * bump version to 2.0.0 in version.json
    
    * Refactor webapp-gcs module configuration to unify service naming and trigger settings

--- terraform/stage-1/prod/main.tf
+++ terraform/stage-1/prod/main.tf
@@ -5,11 +5,11 @@ module "webapp-gcs" {
   project_id    = local.project_id
   region        = local.region
 
-  service_name          = "ll-prod-webapp-gcs"
-  service_account_name  = "ll-prod-build-sa-gcs"
-  branch_pattern        = "^prepare-v2-release$"
-  bucket_name           = "luca-ledger-devops-prod-webapp"
-  trigger_name          = "lucaledger-prod-gcs"
+  service_name          = "lucaledger-prod-webapp"
+  service_account_name  = "lucaledger-prod-build-sa"
+  branch_pattern        = "^main$"
+  bucket_name           = "lucaledger-prod-webapp"
+  trigger_name          = "lucaledger-prod-webapp"
   ssl_domains           = ["lucaledger.app"]
 }
 
@@ -26,19 +26,4 @@ module "webapp-v1" {
   bucket_name           = "lucaledger-prod-webapp-v1"
   trigger_name          = "lucaledger-prod-v1"
   ssl_domains           = ["v1.lucaledger.app"]
-}
-
-module "webapp-v2" {
-  source = "../services/webapp-gcs"
-
-  env         = local.env
-  project_id  = local.project_id
-  region      = local.region
-
-  service_name          = "lucaledger-prod-webapp-v2"
-  service_account_name  = "build-sa-v2"
-  branch_pattern        = "^main$"
-  bucket_name           = "lucaledger-prod-webapp-v2"
-  trigger_name          = "lucaledger-prod-v2"
-  ssl_domains           = ["beta.lucaledger.app"]
 }
